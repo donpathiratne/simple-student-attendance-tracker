6ec36d0d8ea5b47dbbbb719967d3d769ce957c44
Yehan Pathiratne <yehanpathirathne@gmail.com>
2026-01-16 15:53:51 +0530

    Update to get student attendance details part and call functions.py

--- simple_student_attendance_tracker.py
+++ simple_student_attendance_tracker.py
@@ -9,3 +9,29 @@ def tot_student():
         except ValueError:
             print('invalid input')
     return number_all_students
+
+def stu_attendence_func():
+    global count
+    global number_all_students
+    while True:
+        print()
+        student_name= input('Enter Student Name: ')
+        stu_attendence= int(input('Enter student presence or absent(1/0): ')) # get the student present(1) or absent(0)
+        if stu_attendence == 1 or stu_attendence == 0:
+            stu_attendence_dict[student_name] = stu_attendence
+        else:
+            print('Invalid input.Press 1 or 0')
+            continue
+        if count == number_all_students:
+            break
+        count+= 1
+    return stu_attendence_dict
+
+'''call functions and print outputs'''
+number_all_students=tot_student() # call the function 
+stu_attendence_dict= {} # empty dict
+print(f"Present - 1\nAbsent - 0")
+count=1
+stu_attendence_dict=stu_attendence_func()
+stu_attendence_func()
+
